cab98704039088025ac44c12efe0c9eca0b55671
Alex Nor <advance114@gmail.com>
2025-10-09 17:00:29 +0300

    Trash Destruction

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,20 +10,32 @@ public class ApplePicker : MonoBehaviour
     public int numBasket = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBasket; i++) 
         {
             GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (i * basketSpacingY);
             tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
         }
     }
-    
-    void Update()
+
+    public void AppleDestroyd()
     {
-        
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGo in tAppleArray)
+        {
+            Destroy(tGo);
+        }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGo = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGo);
     }
 }
